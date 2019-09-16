4a2c48d4827b339e9725f45bc052b41033fdaaba
Joseph Feiglin <yfeiglin@student.unimelb.edu.au>
2019-09-16 21:47:43 +1000

    working basic - get and put

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,18 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 
-const bodyParser = require('body-parser');
+const body_parser = require('body-parser');
 
 const Firebase = require('./firebase.js')
 
-
+/*
 // Not sure if still needed?
 var cors = require('cors');
 app.use(cors());
+*/
 
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
 
 const API_PORT = 3001;
 
@@ -23,19 +23,19 @@ Create the endpoints for artifacts only -  GET/artifacts, GET/artifacts/<ID>, PU
 */
 
 
-
-
-
 // put method - creating item in database
 router.put('/artifacts', (req,res) => {
     // Input an artifact - using firebase.js
-    //res.send("Storing artifact");
     const {name, description} = req.body;
+    //console.log(req.body);
     if(!name || !description) {
+        console.log('invalid input received')
         return res.json({
             success: false,
+            data: req.body,
             error: 'INVALID INPUTS'
         });
+        
     }
     Firebase.add_new_artifact(req.body).then(updated_artifact_json => {
         return res.json({ 
@@ -44,83 +44,46 @@ router.put('/artifacts', (req,res) => {
         });
     });
 });
-/*
 
-For Testing
-var test_artifact_json = {
-    "name": "IMG5000.jpg",
-    "description": "Photo of Sydney Opera House",
-    "ownerID": "abc123",
-    "originDate": 1548823080,
-    "originLocation": {
-      "lat": -33.855659,
-      "long": 151.210553
-    },
-    "knownLocation": {
-      "lat": -82.468636,
-      "long": -87.998860
-    },
-    "collectionID": 2019,
-    "tags": {
-      "photo": true,
-      "Sydney": true,
-      "architecture": true
-    }
-  };
-  
-Firebase.add_new_artifact(test_artifact_json).then(updated_artifact_json => {
-    console.log(updated_artifact_json.id);
-  });
 
-router.get('/', (req,res) => {
-    console.log('Got a GET');
-    return res.json({
-        success: true,
-        data : "Hello"
-    })
-})*/
+
 
 // the get method - viewing item with specific id in database
 router.get('/artifacts', (req, res) => {
-    res.send("Getting artifact");
+
     const {item_id} = req.body;
+
     if(!item_id) {
+        console.log("Getting everything")
         // Return all items
-        Firebase.fetch_all_artifacts().then(artifact_jsons => {
-            return res.json({
+        Firebase.fetch_all_artifacts().then( artifacts_json => {
+                res.json({ 
                 success: true, 
-                data: artifact_jsons
+                data: artifacts_json
             });
         });
     }
     Firebase.fetch_artifact(item_id).then(artifact_json => {
-        return res.json({
+        console.log('Getting: ' + item_id);
+            res.json({
             success: true, 
             data: artifact_json
         });
     });
 });
 
-
-
-
-
-
 //append /api for our http requests
 app.use('/api', router);
 
 app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.status(500).send('Something broke!')
-  })
-
-
-app.use(function (req, res, next) {
-    res.status(404).send("Sorry can't find that!")
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
   });
 
 
-
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!");
+  });
 
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
