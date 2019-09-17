713da6770138c2985516a5d418180aae87969e4c
Joseph Feiglin <yfeiglin@student.unimelb.edu.au>
2019-09-17 21:10:28 +1000

    turned cors back on

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,8 @@ const body_parser = require('body-parser');
 
 const Firebase = require('./firebase.js')
 
-/*
-// Not sure if still needed?
 var cors = require('cors');
 app.use(cors());
-*/
 
 app.use(body_parser.urlencoded({ extended: false }));
 app.use(body_parser.json());
