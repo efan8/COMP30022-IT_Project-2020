4d3cdcae511d8675d70e919d45ee73bdf0eea0f9
nmontorio98 <43783308+nmontorio98@users.noreply.github.com>
2019-09-19 20:52:13 +1000

    cleaned up code added navbar

--- client/src/Components/AddItem/index.js
+++ client/src/Components/AddItem/index.js
@@ -1,9 +1,12 @@
+/* Add Item form to allow user to input a new item. Still needs
+   to send data to the backend */
+   
 import React from 'react';
-import Styles from '../../style.css';
-import defaultImage from '../../placeholder.png';
+import '../../style.css';
+
+import AddItemComponent from './AddItemComponent';
 
 
-//Ugly add item page. will need to trim into components and style accordingly
 class AddItem extends React.Component {
 
     constructor() {
@@ -13,66 +16,29 @@ class AddItem extends React.Component {
             imageURL: "",
             description: "",
             tags: [],
-            collectionID: "", // might be a number
+            collectionID: "",
             originLocation: "",
             orginDate: "",
-        }
+        };
         this.handleChange = this.handleChange.bind(this);
-    }
+    };
 
     handleChange(event){
-        const {name, type, value} = event.target;
+        const {name, value} = event.target;
         this.setState(
             {[name]: value}
         );
-    }
+    };
 
     render() {
-        return (
-            <form>
-                <h1 className="title">Add Item</h1>
-                <h3 className="heading">Item Name:</h3>
-
-                <input
-                    name="name"
-                    type="textbox"
-                    value={this.state.name}
-                    onChange={this.handleChange}
-                    placeholder="Type item name here"
-                    className="textbox"/>
-                
-                <img src={defaultImage}/>
-                <p>Take Image | Upload Image</p>
-
-                <h3>Description:</h3>
-                <input 
-                    placeholder="Simple Description of  object and stuff"
-                    className="descriptionbox"
-                    name="description"
-                    value={this.state.description}
-                    onChange={this.handleChange}
-                    />
-
-                <h3>Tags:</h3>
-                <button>random tag</button>
-
-                <h3>Collection:</h3>
-                <select>
-                    <option value="collection1">collection1</option>
-                    <option value="collection2">collection2</option>
-                    <option value="collection3">collection3</option>
-                    <option value="newcollection">create new collection</option>
-                </select>
-
-                <h3>Location:</h3>
-                <input placeholder="CurrentLocation" />
-
-                <h3>Origin Date:</h3>
-                <input/> <button>changeDate</button>
-            </form>
-
-        )
+        return(
+            <AddItemComponent 
+                handleChange={this.handleChange} 
+                state={this.state}
+            />
+        );
+        
     };
-}
+};
 
 export default AddItem;
