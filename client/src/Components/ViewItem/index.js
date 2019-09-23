905d4711928c2fb5e02e9dc622d8ac18b587c9c8
nmontorio98 <43783308+nmontorio98@users.noreply.github.com>
2019-09-23 16:23:23 +1000

    view item complete

--- client/src/Components/ViewItem/index.js
+++ client/src/Components/ViewItem/index.js
@@ -19,23 +19,23 @@ class ViewItem extends React.Component {
     // will put a fetch comand in to get data from api
     // Currently using a local json file.
     componentDidMount() {
-        this.setState({loading:true})
-        // fetch("api website")
-        // .then(response => response.json())
-        // .then(data => {
-        //     this.setState({
-        //         item: data
-        //     })
-        // })
-        this.setState({
-            loading: false,
-            item: Data
-        });
+        this.setState({loading: true})
+        fetch("http://localhost:3001/api/artifacts?item_id=-LpBgBzsfMyY41fanxLn")
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+                loading: false,
+                item: data.data
+            })
+            console.log(data.data) 
+        })
+        console.log(this.state.item ? this.state.item.id : "no")
 
     };
 
     render() {
         return(
+            
             <ViewItemComponent state={this.state} />
         );
     };
