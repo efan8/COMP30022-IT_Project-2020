5d5dcd1f647c9b883a89cd4ac9b57541cb3ecb7e
Eric Fan <ericfan898@hotmail.com>
2019-10-02 01:16:48 +1000

    Added front end signup functionality, and cookie creation functionality in backend

--- backend/server.js
+++ backend/server.js
@@ -64,30 +64,38 @@ router.put('/artifacts', (req,res) => {
 
 // the get method - viewing item with specific id in database
 router.get('/artifacts', (req, res) => {
-
-    const item_id = req.query.item_id;
-    //console.log(req);
-    if(!item_id) {
-        console.log("Getting everything");
-        // GET all items
-        Firebase.fetch_all_artifacts().then( artifacts_json => {
-            res.json({
-            success: true,
-            data: artifacts_json});
-        });
-    }
-    else {
-        Firebase.fetch_artifact(item_id).then(artifact_json => {
-            console.log('Getting: ' + item_id);
-            res.json({
-            success: true,
-            data: artifact_json});
-        });
-    }
+    verify_session_cookie(req).then(is_valid_cookie => {
+        if (is_valid_cookie) {
+            console.log("Valid cookie");
+        }
+        else {
+            // Session cookie is unavailable or invalid. Force user to login.
+            console.log("Invalid cookie");
+        }
+        const item_id = req.query.item_id;
+        //console.log(req);
+        if(!item_id) {
+            console.log("Getting everything");
+            // GET all items
+            Firebase.fetch_all_artifacts().then( artifacts_json => {
+                res.json({
+                success: true,
+                data: artifacts_json});
+            });
+        }
+        else {
+            Firebase.fetch_artifact(item_id).then(artifact_json => {
+                console.log('Getting: ' + item_id);
+                res.json({
+                success: true,
+                data: artifact_json});
+            });
+        }
+    });
 });
 
 /*
-The endpoints for authentication requests - PUT/signup
+The endpoints for authentication requests - PUT/signup, POST/login
 */
 
 router.put('/signup', (req,res) => {
@@ -109,8 +117,49 @@ router.put('/signup', (req,res) => {
             });
         });
     }
-
 });
+
+router.post('/login', (req,res) => {
+    // Get the ID token passed and the CSRF token.
+    const idToken = req.body.idToken.toString();
+
+    // Set session expiration to 30 days.
+    const expiresIn = 60 * 60 * 24 * 30 * 1000;
+    // Create the session cookie. This will also verify the ID token in the process.
+    // The session cookie will have the same claims as the ID token.
+    // To only allow session cookie setting on recent sign-in, auth_time in ID token
+    // can be checked to ensure user was recently signed in before creating a session cookie.
+    admin.auth().createSessionCookie(idToken, {expiresIn}).then((sessionCookie) => {
+        // Set cookie policy for session cookie.
+        const options = {maxAge: expiresIn, httpOnly: true, secure: true};
+        res.cookie('session', sessionCookie, options);
+        res.end(JSON.stringify({status: 'success'}));
+    }, error => {
+        res.status(401).send('UNAUTHORIZED REQUEST!');
+    });
+});
+
+/*
+Helper functions
+*/
+
+// Function to verify the session cookie of the HTTP request is valid
+function verify_session_cookie(req) {
+    return new Promise(function(resolve, reject) {
+        const sessionCookie = req.cookies.session || '';
+        // Verify the session cookie. In this case an additional check is added to detect
+        // if the user's Firebase session was revoked, user deleted/disabled, etc.
+        admin.auth().verifySessionCookie(
+            sessionCookie, true /** checkRevoked */)
+            .then((decodedClaims) => {
+                resolve(true);
+            }).catch(error => {
+                // Session cookie is unavailable or invalid. User must re-login.
+                resolve(false);
+            });
+    });
+}
+
 
 //append /api for our http requests
 app.use('/api', router);
--- client/src/Components/SignUp/index.js
+++ client/src/Components/SignUp/index.js
@@ -6,6 +6,11 @@ import '../../style.css';
 import SignUpFormComponent from './SignUpFormComponent';
 import * as firebase from "firebase/app";
 import "firebase/auth";
+import axios from 'axios';
+
+const transport = axios.create({
+  withCredentials: true
+});
 
 const firebaseConfig = {
     apiKey: "AIzaSyDlG7W2KW8AzVM-W5tOYlcrAiH9lDbzv1Y",
@@ -16,6 +21,11 @@ const firebaseConfig = {
     messagingSenderId: "240150750224",
     appId: "1:240150750224:web:b6b663108abd79251e1695"
 };
+
+// Endpoints
+const BACKEND_ROOT = "http://localhost:3001/api/"
+const SIGNUP = BACKEND_ROOT + "signup";
+const LOGIN = BACKEND_ROOT + "login";
 
 firebase.initializeApp(firebaseConfig);
 
@@ -55,13 +65,35 @@ class SignUp extends React.Component {
         }
         else {
             // Sign up this user via backend
-            //
-            // Sign in with email and password, obtaining user's ID token
-            //
-            // Request session cookie from backend, providing ID token
-            //
-            // Clear state, now that a cookie has been obtained
-            //
+            var user = {
+                "firstName": this.state.firstName,
+                "lastName": this.state.lastName,
+                "email": this.state.email,
+                "password": this.state.passwordOne
+            };
+            transport.put(SIGNUP, user).then(res => {
+                console.log(res);
+                var new_user_json = res.data;
+                console.log(new_user_json);
+
+                // Sign in with email and password, obtaining user's ID token
+                return firebase.auth().signInWithEmailAndPassword(user.email, user.password);
+            }).then(user_credential => {
+                // Get the user's ID token as it is needed to exchange for a session cookie.
+                return user_credential.user.getIdToken();
+            }).then(idToken => {
+                console.log("ID Token: " + idToken);
+                // Session login endpoint in backed is queried and the session cookie is set.
+                return transport.post(LOGIN, idToken);
+            }).then(() => {
+                // A page redirect would suffice as the persistence is set to NONE.
+                return firebase.auth().signOut();
+            }).then(() => {
+                // Navigate to Welcome page
+                //
+                //
+                console.log("Signed up and signed in! Navigating to welcome page now...")
+            });
         }
         event.preventDefault();
     }
