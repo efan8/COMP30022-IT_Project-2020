c93c6dc45014185ab8f5e37613cf01ffda3c87da
Eric Fan <ericfan898@hotmail.com>
2019-10-01 23:28:45 +1000

    Adding sign up functionality framework

--- client/src/Components/SignUp/index.js
+++ client/src/Components/SignUp/index.js
@@ -4,9 +4,26 @@
 import React from 'react';
 import '../../style.css';
 import SignUpFormComponent from './SignUpFormComponent';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDlG7W2KW8AzVM-W5tOYlcrAiH9lDbzv1Y",
+    authDomain: "it-project-2019sem2.firebaseapp.com",
+    databaseURL: "https://it-project-2019sem2.firebaseio.com",
+    projectId: "it-project-2019sem2",
+    storageBucket: "",
+    messagingSenderId: "240150750224",
+    appId: "1:240150750224:web:b6b663108abd79251e1695"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// As httpOnly cookies are to be used, do not persist any state client side.
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
 
 class SignUp extends React.Component {
-    
+
     constructor() {
         super();
         this.state ={
@@ -27,10 +44,32 @@ class SignUp extends React.Component {
         );
     };
 
+    handleSubmit(event) {
+        if (this.state.firstName == "" || this.state.lastName == "" ||
+            this.state.email == "" || this.state.passwordOne == "" ||
+            this.state.passwordTwo == "") {
+            this.setState ({output : [<p>Please fill in all details before submitting</p>]});
+        }
+        else if (this.state.passwordOne != this.state.passwordTwo) {
+            this.setState ({output : [<p>Passwords do not match</p>]});
+        }
+        else {
+            // Sign up this user via backend
+            //
+            // Sign in with email and password, obtaining user's ID token
+            //
+            // Request session cookie from backend, providing ID token
+            //
+            // Clear state, now that a cookie has been obtained
+            //
+        }
+        event.preventDefault();
+    }
+
     render() {
         return(
-            <SignUpFormComponent 
-                data={this.state} 
+            <SignUpFormComponent
+                data={this.state}
                 handleChange={this.handleChange}
             />
         );
