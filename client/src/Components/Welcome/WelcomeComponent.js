0584a9e46d3735ae5a4d28ee28615f174e502bee
Jibby98 <30643564+Jibby98@users.noreply.github.com>
2019-10-02 18:55:03 +1000

    Sorting (Name ascendind and descending)

--- client/src/Components/Welcome/WelcomeComponent.js
+++ client/src/Components/Welcome/WelcomeComponent.js
@@ -8,6 +8,42 @@ function WelcomeComponent(props){
     let data = props.state.item.data;
     let search = props.state.search;
 
+    //Sorting of items
+    //Old and new case are kinda broken need to fix
+    function sortData(sortType) {
+        switch(sortType) {
+            // case "old":
+            //     data.sort(function(a, b) {
+            //         var orderBool = a.dataAdded > b.dataAdded;
+            //         return orderBool ? 1 : -1;
+            //     });
+            //     break;
+            // case "new":
+            //     data.sort(function(a, b) {
+            //         var orderBool = a.dataAdded < b.dataAdded;
+            //         return orderBool ? 1 : -1;
+            //     });
+            //     break;
+            case "nameDesc":
+                data.sort(function(a, b) {
+                    var orderBool = a.name.toLowerCase() > b.name.toLowerCase();
+                    return orderBool ? 1 : -1;
+                });
+                break;
+            case "nameAsc":
+                data.sort(function(a, b) {
+                    var orderBool = a.name.toLowerCase() < b.name.toLowerCase();
+                    return orderBool ? 1 : -1;
+                });
+                break;
+            default:
+                break;
+        }
+    }
+    sortData(props.state.selectedOption);
+
+
+
     //Creates an array of all items needing to be displayed and puts them
     //into a list of items
     let items = [];
@@ -48,6 +84,14 @@ function WelcomeComponent(props){
                     placeholder="Search"
                 />
                 <input type="submit"/>
+                <p> Sort by:
+                <select onChange={props.handleSelectChange} name="sort">
+                    {/* <option value="old">Oldest</option>
+                    <option value="new">Newest</option> */}
+                    <option value="nameDesc">Name A-Z</option>
+                    <option value="nameAsc">Name Z-A</option>
+                </select>
+                </p>
             </form>
             <p>{items}</p>
         </div>
