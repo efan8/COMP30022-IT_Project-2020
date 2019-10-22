cf1067294cbd77d85e482f8bd93a2252dbf84cd3
Eric Fan <ericfan898@hotmail.com>
2019-10-23 03:58:55 +1100

    Added custom font, and implemented flexible nav bar

--- client/src/Components/NavigationBar/index.js
+++ client/src/Components/NavigationBar/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { logout } from '../Auth/auth';
 
-
 class NavigationBar extends React.Component {
 
     constructor() {
@@ -11,28 +10,53 @@ class NavigationBar extends React.Component {
         this.state ={
             name: ""
         };
+        this.toggleMenu = this.toggleMenu.bind(this);
+        this.toggleMenuThenLogout = this.toggleMenuThenLogout.bind(this);
     };
-    
+
     navigationHandle() {
-        
+
     }
+
+    toggleMenu(event) {
+        let mainNavLinks = document.getElementById('main-nav-links');
+        console.log(mainNavLinks.classList);
+        mainNavLinks.classList.toggle('active');
+    }
+
+    toggleMenuThenLogout(event) {
+        let mainNavLinks = document.getElementById('main-nav-links');
+        console.log(mainNavLinks.classList);
+        mainNavLinks.classList.toggle('active');
+        logout();
+    }
+
     render() {
         return(
             <header className="mainNavigation">
-                <div className="mainNavigationLogo">
-                    <h1>FridaySlackers</h1>
-                </div>
                 <nav className="mainNavigationItems">
+                <span className="mainNavigationLogo">
+                    <h1>FridaySlackers</h1>
+                </span>
+                    <span onClick={this.toggleMenu} className="navbar-toggle" id="navbar-toggle">
+                        <svg id="nav-menu-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="20" height="20">
+                            <path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z"/>
+                        </svg>
+                    </span>
+                    <span className="main-nav-links" id="main-nav-links">
                     <ul>
-                        <li><NavLink to="/LandingPage">Home</NavLink></li>
-                        <li><NavLink to="/Welcome">Welcome</NavLink></li>
-                        <li><NavLink to="/AddItem">Add Item</NavLink></li>
+                        <li><NavLink onClick={this.toggleMenu} to="/LandingPage">Home</NavLink></li>
+                        <li><NavLink onClick={this.toggleMenu} to="/Welcome">Welcome</NavLink></li>
+                        <li><NavLink onClick={this.toggleMenu} to="/AddItem">Add Item</NavLink></li>
+                        <li>
+                            <NavLink className="logout" to="/LandingPage"
+                                onClick={this.toggleMenuThenLogout}>Logout
+                            </NavLink>
+                        </li>
                     </ul>
+
+                    </span>
                 </nav>
-                <NavLink className="logout"
-                    to="/LandingPage" 
-                    onClick={logout}>Logout
-                </NavLink>
             </header>
         )
     }
